af7871ea96bf9f86bd937965786feff752acc791
Dana Tal <dana-tal@012.net.il>
2025-02-12 20:58:21 +0200

    small fix

--- part_1/course_info/src/App.jsx
+++ part_1/course_info/src/App.jsx
@@ -12,7 +12,7 @@ const Part = (props) =>{
 const Content = (props) =>{
        
      return  <> 
-       { props.parts.map((part,index)=>(<Part part={part}  exercise={props.exercises[index]} />)) }    
+       { props.parts.map((part,index)=>(<Part key={index} part={part}  exercise={props.exercises[index]} />)) }    
        </>;
 }
 
